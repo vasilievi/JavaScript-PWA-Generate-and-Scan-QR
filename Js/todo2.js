41c997d427f6bd7ac766aefa7df911cf08a8f611
Igor <vasilievi@outlook.com>
2021-01-06 00:15:47 +0300

    scanner

--- Js/todo2.js
+++ Js/todo2.js
@@ -61,7 +61,19 @@ inputLoad.addEventListener('click', function () {
   });
 })
 
-
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+scanner.addListener('scan', function (content) {
+  console.log(content);
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    scanner.start(cameras[0]);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function (e) {
+  console.error(e);
+});
 
 
 /* input.addEventListener("keypress", function (keyPressed) {
